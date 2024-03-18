b6efdef14b32b27b6c643764b531dcd6c0744938
tanisha0605 <tanishakanal@gmail.com>
2024-03-18 15:55:31 +0530

    Dashboard changes

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,13 +1,21 @@
 import { useState, useRef, useEffect } from "react";
 import Nav from "@/components/Navbar";
+import  Card  from "@/components/Card";
+import Card2 from "@/components/Card2";
 import { GLOBAL_VARIABLE_SATISFIED } from '@/../config';
 import { GLOBAL_VARIABLE_NOT_SATISFIED } from '@/../config';
 export default function Dashboard(){
         return(
             <div>
+            <div>
                 <Nav/>
             </div>
+            <div >
+                <Card />
+                <Card2/>
+            </div>
             
+            </div>
     
         )
 }

74566b242c644eceed7b6b06e802ee1741b514f2
surabhiwaingankar <surabhiwaingankar@gmail.com>
2024-03-18 15:56:34 +0530

    added files

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -3,6 +3,29 @@ import Nav from "@/components/Navbar";
 import { GLOBAL_VARIABLE_SATISFIED } from '@/../config';
 import { GLOBAL_VARIABLE_NOT_SATISFIED } from '@/../config';
 export default function Dashboard(){
+    const mappedArray1 = GLOBAL_VARIABLE_SATISFIED.map((innerArray, index) => {
+        const userString = `User ${index + 1}`;
+        
+        // Return an object containing the user string and the inner array
+        return {
+            user: userString,
+            innerArray: innerArray
+        };
+    });
+
+    const mappedArray2 = GLOBAL_VARIABLE_NOT_SATISFIED.map((innerArray, index) => {
+        const userString = `User ${index + 1}`;
+        
+        // Return an object containing the user string and the inner array
+        return {
+            user: userString,
+            innerArray: innerArray
+        };
+    });
+    
+    console.log(mappedArray1);
+    
+    console.log(mappedArray2);
         return(
             <div>
                 <Nav/>

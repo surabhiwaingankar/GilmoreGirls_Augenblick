cc7965b917a7b41d5060d4b2f4172ca6aa192504
tanisha0605 <tanishakanal@gmail.com>
2024-03-18 15:58:38 +0530

    Dashboard changes

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -5,17 +5,15 @@ import Card2 from "@/components/Card2";
 import { GLOBAL_VARIABLE_SATISFIED } from '@/../config';
 import { GLOBAL_VARIABLE_NOT_SATISFIED } from '@/../config';
 export default function Dashboard(){
-        return(
-            <div>
-            <div>
-                <Nav/>
-            </div>
-            <div >
-                <Card />
-                <Card2/>
-            </div>
-            
-            </div>
-    
-        )
+    return (
+        <div>
+          <div>
+            <Nav />
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Card />
+            <Card2 />
+          </div>
+        </div>
+      );
 }
